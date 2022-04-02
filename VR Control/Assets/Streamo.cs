f4257364b5e8c96def8b8b4ca0a1a7c4b3fe3005
Dario Lencina Talarico <dario@armore.dev>
2022-04-02 15:20:18 -0400

    working

---
+++ VR Control/Assets/Streamo.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Netcode.Transports.WebSocket;
+
+using UnityEngine;
+
+public class VideoPacket
+{
+    public string data;
+    public string frameType;
+    public int epochTime;
+    public string encoding;
+}
+
+public class Streamo : MonoBehaviour
+{
+    // Rigidbody variable to hold the player ball's rigidbody instance
+    private SpriteRenderer s;
+
+    private IWebSocketClient client;
+
+    private string url = "ws://192.168.18.11:8080/ws";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        s = GetComponent<SpriteRenderer>();
+        client = WebSocketClientFactory.Create(url);
+        client.Connect();
+        Debug.Log("[Streamo] on create");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (client == null)
+        {
+            client = WebSocketClientFactory.Create(url);
+            client.Connect();
+            Debug.Log("client is null, c# sucks");
+            return;
+        }
+        WebSocketEvent wsEvent = client.Poll();
+        Debug.Log("ws event " + wsEvent.Type);
+        if (wsEvent.Type == WebSocketEvent.WebSocketEventType.Payload)
+        {
+            Debug.Log("got data!");
+            string result = System.Text.Encoding.UTF8.GetString(wsEvent.Payload);
+            VideoPacket packet = JsonUtility.FromJson< VideoPacket>(result);
+            Debug.Log("parsed data!" + packet);
+            byte[] data = System.Convert.FromBase64String(packet.data);
+            Texture2D tex = new Texture2D(2, 2);
+            tex.LoadImage(data); 
+            s.material.mainTexture = tex;
+        }
+
+        if (wsEvent.Type == WebSocketEvent.WebSocketEventType.Error)
+        {
+            Debug.Log("error " + wsEvent.Error);
+        }
+
+        if (wsEvent.Type == WebSocketEvent.WebSocketEventType.Open)
+        {
+            Debug.Log("ws connected");
+        }
+
+    }
+}
